f8316edae9ff0f940e38ef99e7d8b226b7fc0d14
jgmonferrer <146870285+jgmonferrer@users.noreply.github.com>
2023-11-29 15:13:42 +0100

    Add files via upload

---
+++ Fase-4-Subsistema-Jaume-I/Fase4.1/menuBusquedaObjetos.ps1
@@ -0,0 +1,78 @@
+
+function Show-Menu
+{
+     param (
+           [string]$Titulo = 'Men煤 principal'
+     )
+     Clear-Host
+     Write-Host "================ $Titulo ================"
+    
+     Write-Host "1: Opcion '1' Buscar todas las UOs."
+     Write-Host "2: Opci髇  '2' Buscar todos los Equipos."
+     Write-Host "3: Opcion '3' Buscar todos los Grupos."
+     Write-Host "4: Opci贸n '4' Buscar todos los Usuarios."
+     Write-Host "Q: Opci贸n 'Q' Salir."
+}
+function busqueda_UOs
+{
+#Listar los usuarios del dominio smr.local
+Write-Host UOs -Fore green 
+Get-ADOrganizationalUnit -filter * -SearchBase "dc=IESJaume-I,dc=mylocal" | Format-Table Name
+}
+function busqueda_grupos
+{
+#Listar los usuarios del dominio smr.local
+Write-Host Grupos -Fore green 
+Get-ADGroup -filter * -SearchBase "dc=IESJaume-I,dc=mylocal" | Format-Table Name
+}
+function busqueda_usuarios
+{
+#Listar los usuarios del dominio smr.local
+Write-Host usuarios -Fore green 
+Get-ADUser -filter * -SearchBase "dc=IESJaume-I,dc=mylocal" | Format-Table Name
+}
+
+function busqueda_equipos
+{
+#Listar los usuarios del dominio smr.local
+Write-Host Equipos -Fore green 
+Get-ADComputer -filter * -SearchBase "dc=IESJaume-I,dc=mylocal" | Format-Table Name
+}
+
+#Primero comprobaremos si se tiene cargado el m贸dulo Active Directory
+if (!(Get-Module -Name ActiveDirectory)) #Acceder谩 al then solo si no existe una entrada llamada ActiveDirectory
+{
+  Import-Module ActiveDirectory #Se carga el m贸dulo
+}
+
+
+#
+#MENU PRINCIPAL
+#
+do
+{
+     Show-Menu
+     $input = Read-Host "Por favor, pulse una opcion"
+     switch ($input)
+     {
+           '1' {
+                Clear-Host
+                busqueda_UOs
+           } '2' {
+                Clear-Host
+                busqueda_equipos
+           } '3' {
+                Clear-Host
+                busqueda_grupos
+           } '4' {
+                Clear-Host
+                busqueda_usuarios
+           } 'q' {
+                'Salimos de la App'
+                return
+           }
+     }
+     pause
+}
+until ($input -eq 'q')
+
---
+++ Fase-4-Subsistema-Jaume-I/Fase4.1/menuGestionObjetos.ps1
@@ -0,0 +1,163 @@
+#Variables globales
+$domain="dc=IESJaume-I,dc=mylocal"
+#
+#Funciones en la cabecera del script
+#
+
+function Show-Menu
+{
+     param (
+           [string]$Titulo = 'Menú principal'
+     )
+     Clear-Host
+     Write-Host "================ $Titulo ================"
+    
+     Write-Host "1: Opción '1' Crear UOs."
+     Write-Host "2: Opción '2' Crear Equipos."
+     Write-Host "3: Opción '3' Crear Grupos."
+     Write-Host "4: Opción '4' Crear Usuarios."
+     Write-Host "5: Opción '5' Añadir miembros a grupos"
+     Write-Host "Q: Opción 'Q' Salir."
+}
+function alta_UOs
+{
+     $ficheroCsvUO=Read-Host "Introduce el fichero csv de UO's:"
+     $ficheroImportado = import-csv -Path $ficheroCsvUO -delimiter *
+     foreach($line in $ficheroImportado)
+     {
+          New-ADOrganizationalUnit -Description:$line.Description -Name:$line.Name `
+		-Path:$line.Path -ProtectedFromAccidentalDeletion:$true 
+     }
+     Write-Host "Se han creado las UOs satisfactoriamente en el dominio $domain"
+}
+function alta_grupos
+{
+     	$gruposCsv=Read-Host "Introduce el fichero csv de Grupos:"
+	$ficheroImportado = import-csv -Path $gruposCsv -delimiter *
+	foreach($linea in $ficheroImportado)
+	{
+		if ( !(Get-ADGroup -Filter 'name -eq "$linea.Name"' ) )
+		{
+			New-ADGroup -Name:$linea.Name -Description:$linea.Description `
+			-GroupCategory:$linea.Category `
+			-GroupScope:$linea.Scope  `
+			-Path:$linea.Path
+		}	
+		else { Write-Host "El grupo $line.Name ya existe en el sistema"}
+	}
+ 	 Write-Host "Se han creado las grupos satisfactoriamente en el dominio $domain"
+     
+}
+function alta_usuarios
+{
+	$fileUsersCsv=Read-Host "Introduce el fichero csv de los usuarios:"
+	$ficheroImportado = import-csv -Path $fileUsersCsv -Delimiter *				     
+	foreach($linea in $ficheroImportado)
+	{
+		$passAccount=ConvertTo-SecureString $linea.Password -AsPlainText -force
+		$Surnames=$linea.Surname1+' '+$linea.Surname2
+		$nameLarge=$linea.Name+' '+$linea.Surname1+' '+$linea.Surname2
+		$email=$linea.Email
+		[boolean]$Habilitado=$true
+    		If($linea.Enabled -Match 'N') { $Habilitado=$false}
+		#Establecer los dÃ­as de expiraciÃ³n de la cuenta (Columna del csv ExpirationAccount)
+   		$ExpirationAccount = $linea.TurnPassDays
+    		$timeExp = (get-date).AddDays($ExpirationAccount)
+		#
+		# Ejecutamos el comando para crear el usuario
+		#
+		New-ADUser -SamAccountName $linea.Account -UserPrincipalName $linea.Account -Name $linea.Account `
+			-Surname $Surnames -DisplayName $nameLarge -GivenName $linea.Name -LogonWorkstations:$linea.Computer `
+			-Description "Cuenta de $nameLarge" -EmailAddress $email `
+			-AccountPassword $passAccount -Enabled $Habilitado `
+			-CannotChangePassword $false -ChangePasswordAtLogon $true `
+			-PasswordNotRequired $false -Path $linea.Path -AccountExpirationDate $timeExp `
+   			-ScriptPath $linea.ScriptPath `
+		
+  		#
+  		## Establecer horario de inicio de sesiÃ³n       
+                $horassesion = $linea.NetTime -replace(" ","")
+                net user $linea.Account /times:$horassesion 
+		
+  		#Asignar cuenta de Usuario a Grupo
+		# Distingued Name CN=Nombre-grupo,ou=..,ou=..,dc=..,dc=...
+		#En este caso el grupo se encuentra en la misma UO que el usuario
+                $cnGrpAccount=$linea.Group
+		Add-ADGroupMember -Identity $cnGrpAccount -Members $linea.Account
+		
+	}     
+}
+
+
+#Primero comprobaremos si se tiene cargado el mÃ³dulo Active Directory
+if (!(Get-Module -Name ActiveDirectory)) #AccederÃ¡ al then solo si no existe una entrada llamada ActiveDirectory
+{
+  Import-Module ActiveDirectory #Se carga el mÃ³dulo
+}
+function alta_equipos {
+#
+#CreaciÃ³n de los grupos a partir de un fichero csv
+#
+#Lee el fichero grupos.csv. El carÃ¡cter delimitador de columna es :
+$equiposCsv=Read-Host "Introduce el fichero csv de Equipos:"
+$fichero= import-csv -Path $equiposCsv -delimiter "*"
+
+foreach($line in $fichero)
+{
+	#Comprobamos que no exista el equipo en el sistema
+	if ( !(Get-ADComputer -Filter 'name -eq "$line.Computer"' ) )
+	{
+		New-ADComputer -Enabled:$true -Name:$line.Computer -Path:$line.Path -SamAccountName:$line.Computer
+	}
+	else { Write-Host "El equipo $line.Computer ya existe en el sistema"}
+}
+
+write-Host ""
+write-Host "Se han creado los equipos en el dominio $domain" -Fore green
+write-Host "" 
+}
+
+function asignacion_miembros
+{
+     	$gruposCsv=Read-Host "Introduce el fichero csv de Grupos:"
+	$ficheroImportado = import-csv -Path $gruposCsv -delimiter *
+	foreach($linea in $ficheroImportado)
+	{
+	 Add-ADGroupMember -Identity $linea.Group -Members $linea.Member 
+	}	
+     
+}
+
+#
+#MENU PRINCIPAL
+#
+do
+{
+     Show-Menu
+     $input = Read-Host "Por favor, pulse una opciÃ³n"
+     switch ($input)
+     {
+           '1' {
+                Clear-Host
+                alta_UOs
+           } '2' {
+                Clear-Host
+                alta_equipos
+           } '3' {
+                Clear-Host
+                alta_grupos
+           } '4' {
+                Clear-Host
+                alta_usuarios
+	   } '5' {
+    		Clear-Host
+      		asignacion_miembros
+           } 'q' {
+                'Salimos de la App'
+                return
+           }
+     }
+     pause
+}
+until ($input -eq 'q')
+

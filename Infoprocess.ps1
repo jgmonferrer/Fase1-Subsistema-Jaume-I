8cb675c5236e6182457ee7fb68ee6dec7793dcf3
jgmonferrer <146870285+jgmonferrer@users.noreply.github.com>
2023-12-17 15:33:21 +0100

    Delete Infoprocess.ps1

--- Infoprocess.ps1
+++
@@ -1,9 +0,0 @@
-$date= Get-Date
-"Fecha: $date" | out-file C:\Logs\Infoprocess.dat -append
-"Procesos consumiendo mas de 25MB de memoria virtual" | out-file C:\Logs\Infoprocess.dat -append
-Get-process | where {$_.virtualmemorysize -gt 500000000} | out-file C:\Logs\Infoprocess.dat -append
-"Procesos consumiendo mas de 50MB de memoria para trabajar" | out-file C:\Logs\Infoprocess.dat -append
-get-process | where {$_.WS -gt 50MB} | out-file C:\Logs\Infoprocess.dat -append
-"Procesos consumiendo mas del 5% de la CPU" | out-file C:\Logs\Infoprocess.dat -append
-Get-process | where-object {$_.CPU -gt 5} | out-file C:\Logs\Infoprocess.dat -append
-
